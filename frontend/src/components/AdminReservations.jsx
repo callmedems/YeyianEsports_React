f4adc7a57aa4e891e9e1499bd051c736ab5fa163
Ana Paola Jiménez Sedano <143899647+AnaJmz345@users.noreply.github.com>
2025-06-11 23:32:15 -0600

    ya detecta que vista está selecccionada

--- frontend/src/components/AdminReservations.jsx
+++ frontend/src/components/AdminReservations.jsx
@@ -3,26 +3,47 @@ import React, { useState, useEffect} from 'react';
 import '../css/adminReservations.css';
 
 export default function AdminReservations(){
-   
+    const [selectedFilter, setSelectedFilter] = useState('Activas'); //para ver qué reservas quiere ver el admin en ese momento (si activas, canceladas, futuras o pasadas)
+    const handleSelectedFilter = (filter) => {
+        setSelectedFilter(filter);
+        console.log("Filtro seleccionado:", filter);
+        // Aquí después puedes hacer el fetch a tu backend con este filtro
+    }
 
     return(         
     <div className="containerAdminReserv">
         <main>
             <h2>Reservas de clientes</h2>
             <div className="chooseResToWatch">
-                <div className="activeOp">Activas</div>
-                <div className="futureOp">Futuras</div>
-                <div className="cancelledOp">Canceladas</div>
-                <div className="pastOp">Pasadas</div>
+                <div  
+                    className={selectedFilter === 'Activas' ? "filterOption activeFilter" : "filterOption"}
+                    onClick={() => handleSelectedFilter('Activas')} 
+                > Activas</div> {/*aqui si el filtro seleccionado fue activas, le pone la clase activeFilter para cambiar el css*/}
+
+                <div
+                    className={selectedFilter === 'Futuras' ? "filterOption activeFilter" : "filterOption"}
+                    onClick={() => handleSelectedFilter('Futuras')}
+                > Futuras</div>
+
+                <div
+                    className={selectedFilter === 'Canceladas' ? "filterOption activeFilter" : "filterOption"}
+                    onClick={() => handleSelectedFilter('Canceladas')}
+                >Canceladas</div>
+
+                <div
+                    className={selectedFilter === 'Pasadas' ? "filterOption activeFilter" : "filterOption"}
+                    onClick={() => handleSelectedFilter('Pasadas')}
+                >Pasadas</div>
+
             </div>
 
             <div className="reservationsTable">
                 <div className="columnsNames">
-                    <div className="nameTC"><i class="bi bi-person"></i>Nombre</div>
-                    <div className="dateTC"><i class="bi bi-calendar2"></i>Fecha</div>
-                    <div className="hourTC"><i class="bi bi-clock"></i>Hora</div>
-                    <div className="resTypeTC"><i class="bi bi-list-stars"></i>Tipo reserva</div>
-                    <div className="cancelTC"><i class="bi bi-x-square"></i>Cancelar</div>
+                    <div className="nameTC"><i className="bi bi-person"></i>Nombre</div>
+                    <div className="dateTC"><i className="bi bi-calendar2"></i>Fecha</div>
+                    <div className="hourTC"><i className="bi bi-clock"></i>Hora</div>
+                    <div className="resTypeTC"><i className="bi bi-list-stars"></i>Tipo reserva</div>
+                    <div className="cancelTC"><i className="bi bi-x-square"></i>Cancelar</div>
                   
                 </div>
 
